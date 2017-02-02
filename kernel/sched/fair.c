derkling/linux

Experimental!: sched/fair: Add energy_diff dead-zone margin
It is not worth the overhead to migrate tasks for tiny insignificant
energy savings. To prevent this, an energy margin is introduced in
energy_diff() which effectively adds a dead-zone that rounds tiny energy
differences to zero. Since no scale is enforced for energy model data
the margin can't be absolute. Instead it is defined as +/-1.56% energy
saving compared to the current total estimated energy consumption.

[rebase: on top of msm-google/android-msm-marlin-3.18,
         massage original patch which changes code in energy_diff()
	 into __energy_diff() introduced by SchedTune]
Signed-off-by: Dietmar Eggemann <dietmar.eggemann@arm.com>

kernel/sched/fair.c
@@ -5009,6 +5009,7 @@ static inline int __energy_diff(struct energy_env *eenv)
 	struct sched_domain *sd;
 	struct sched_group *sg;
 	int sd_cpu = -1;
+	int diff, margin;
 
 	if (eenv->src_cpu == eenv->dst_cpu)
 		return 0;
@@ -5034,6 +5035,16 @@ static inline int __energy_diff(struct energy_env *eenv)
 
 	trace_sched_energy_diff(eenv);
 	trace_sched_energy_perf_deltas(eenv);
+
+	/*
+	 * Dead-zone margin preventing too many migrations.
+	 */
+
+	margin = eenv->before.energy >> 6; /* ~1.56% */
+
+	diff = eenv->after.energy - eenv->before.energy;
+
+	eenv->nrg_delta = (abs(diff) < margin) ? 0 : eenv->nrg_delta;
 
 	return eenv->nrg_delta;
 }

VertexOS-Devices/android_kernel_oneplus_msm8996

Signed-off-by: Morten Rasmussen <morten.rasmussen@arm.com>
Signed-off-by: Chris Redpath <chris.redpath@arm.com>
Change-Id: Ifb27af32bce1b1dfaf1e5375452328836d968dc6

kernel/sched/fair.c
@@ -4928,6 +4928,7 @@ static inline int __energy_diff(struct energy_env *eenv)
 	struct sched_domain *sd;
 	struct sched_group *sg;
 	int sd_cpu = -1, energy_before = 0, energy_after = 0;
+	int diff, margin;
 
 	struct energy_env eenv_before = {
 		.util_delta	= 0,
@@ -4976,6 +4977,16 @@ static inline int __energy_diff(struct energy_env *eenv)
 			eenv->nrg.before, eenv->nrg.after, eenv->nrg.diff,
 			eenv->cap.before, eenv->cap.after, eenv->cap.delta,
 			eenv->nrg.delta, eenv->payoff);
+
+	/*
+	 * Dead-zone margin preventing too many migrations.
+	 */
+
+	margin = eenv->nrg.before >> 6; /* ~1.56% */
+
+	diff = eenv->nrg.after - eenv->nrg.before;
+
+	eenv->nrg.diff = (abs(diff) < margin) ? 0 : eenv->nrg.diff;
 
 	return eenv->nrg.diff;
 }